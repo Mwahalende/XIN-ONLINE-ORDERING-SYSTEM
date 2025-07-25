c17b33e524c45d06e00c74e7368cde6fdada289c
Leo mwahalende <166632518+Mwahalende@users.noreply.github.com>
2025-07-26 00:30:32 +0300

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
-  secret: '1234ghjkgytcf',
+  secret: '12345678',
   resave: false,
   saveUninitialized: false
 }));
